a8d2089f3256d05a2fa970133c41552582b3f953
Equinoxless <glebmaks502@gmail.com>
2023-05-09 18:37:50 +0300

    Cardrow is now a class with 2 functions

---
+++ src/main/java/com/example/blackjackfront/FrontEnd.java
@@ -0,0 +1,48 @@
+package com.example.blackjackfront;
+
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.List;
+
+
+public class FrontEnd extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        // Create the root layout (green table)
+        BorderPane tableLayout = new BorderPane();
+        tableLayout.setPadding(new Insets(10));
+        tableLayout.setStyle("-fx-background-color: green");
+
+        CardHolder ch = new CardHolder(1);
+        List<Card> cards = ch.getCards();
+        FrontRow fr = new FrontRow(cards);
+        fr.makeBotRow(tableLayout);
+        fr.makeTopRow(tableLayout);
+
+        // Create the scene and show the stage
+        Scene scene = new Scene(tableLayout, 800, 600);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Blackjack Table");
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ src/main/java/com/example/blackjackfront/FrontRow.java
@@ -0,0 +1,74 @@
+package com.example.blackjackfront;
+
+import javafx.geometry.Pos;
+import javafx.scene.Group;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+
+import java.io.File;
+import java.util.List;
+
+public class FrontRow {
+
+    List<Card> cards;
+
+    public FrontRow(List<Card> cards) {
+        this.cards = cards;
+    }
+
+    public HBox makeRow(){
+
+        // Create a row of cards and add stuff to it
+        HBox cardrow = new HBox(10);
+        for (Card c : cards) {
+            VBox cardContent = new VBox();
+            Group cardPic = new Group();
+
+            Text valueText = new Text(c.getRank());
+            valueText.setFont(Font.font("Arial", FontWeight.BOLD, 50));
+
+            String suitFile;
+            switch (c.getSuit()) {
+                case "♣" -> suitFile = "clubs.png";
+                case "♦" -> suitFile = "diamonds.png";
+                case "♠" -> suitFile = "spades.png";
+                case "♥" -> suitFile = "hearts.png";
+                default -> suitFile = null;
+            }
+            File suitImageFile = new File(suitFile);
+            String imagePath = suitImageFile.toURI().toString();
+            ImageView suitImage = new ImageView(new Image(imagePath));
+            suitImage.setFitWidth(50);
+            suitImage.setFitHeight(50);
+
+            cardContent.getChildren().addAll(valueText, suitImage);
+
+            Rectangle background = new Rectangle(100, 150, Color.GHOSTWHITE);
+
+            cardPic.getChildren().addAll(background, cardContent);
+
+            cardrow.getChildren().add(cardPic);
+        }
+        return cardrow;
+    }
+    public void makeBotRow(BorderPane table){
+        HBox cardrow = this.makeRow();
+        // Position the card group at the bottom center of the table
+        BorderPane.setAlignment(cardrow, Pos.CENTER);
+        table.setBottom(cardrow);
+    }
+    public void makeTopRow(BorderPane table){
+        HBox cardrow = this.makeRow();
+        // Position the card group at the bottom center of the table
+        BorderPane.setAlignment(cardrow, Pos.CENTER);
+        table.setTop(cardrow);
+    }
+}
